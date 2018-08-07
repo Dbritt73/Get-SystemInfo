0689f5a4920596b3afa54e010b176c400b267168
Darrin Britton <brittod@wwu.edu>
2018-08-07 07:34:55 -0700

    initial commit on systeminfo class

---
+++ SystemInfo/SystmeInfo-Class.ps1
@@ -0,0 +1,35 @@
+Class SystemInformation {
+    
+    [String]$ComputerName
+    [String]$OSName
+    [string]$OSMfgr
+    [String]$OSBuildType
+    [string]$RegisteredOwner
+    [strring]$RegisteredOrganization
+    [string]$ProductSerialNumber
+    [string]$ProductID
+    [datetime]$InstallDate
+    [datetime]$LastBootUpTime
+    [string]$Manufacturer
+    [string]$Model
+    [string]$SystemType
+    [String]$Processors
+    [string]BIOSVersion
+    [string]$WindowsDirectory
+    [string]$SystemDirectory
+    [String]$BootDevice
+    [string]$SystemLocale
+    [string]$InputLocale
+    [string]$TimeZone
+    [string]$TotalPhysicalMemory
+    [string]$AvailablePhysicalMemory
+    [string]$MaximumVirtualMemory
+    [string]$AvailableVirtualMemory
+    [string]$UsedVirtualMemory
+    [string]$PageFiles
+    [string]$Domain
+    [string]$LogonServers
+    [string]$HotFixes
+    [string]$NetworkInformation
+
+}
